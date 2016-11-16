3baf9f5f8561c558510fab1454bea32d3a78fc25
juliani2 <julianezequielmoreno@gmail.com>
2016-11-15 23:00:03 -0300

    Trabajo Practico: comienzo. Falta printf y corregir tiempo de tareas

---
+++ examples/trabajo_practico_str/inc/principal.h
@@ -0,0 +1,23 @@
+#ifndef _BLINKING_H_
+#define _BLINKING_H_
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Blinking Blinking example header file
+ ** @{ */
+/*
+ * Initials     Name
+ * ---------------------------
+ * Supramy       Julian Moreno
+ */
+/*==================[inclusions]=============================================*/
+#include "../../trabajo_practico_str/inc/board.h"
+/*==================[macros]=================================================*/
+/*==================[typedef]================================================*/
+/*==================[external data declaration]==============================*/
+/*==================[external functions declaration]=========================*/
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _BLINKING_H_ */
+
